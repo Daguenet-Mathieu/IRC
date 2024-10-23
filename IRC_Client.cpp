a4ea38aeb3304317e073ab8aba83a49c05938339
madaguen lab <madaguen@made-f0cr9s2.paris.europe.42.school>
2024-10-23 18:30:47 +0200

    ca soket

--- IRC_Client.cpp
+++ IRC_Client.cpp
@@ -1,23 +1,25 @@
 #include "IRC_Client.hpp"
 
-IRC_Client::IRC_Client(int port)
-{	
-    _socket_client = socket(AF_INET, SOCK_STREAM, 0);
-	if (_socket_client== -1)
-		throw ThrowException("ERROR SOCKET");
-	memset(&_client_addr, 0, sizeof(_client_addr));
-	_client_addr.sin_family = AF_INET;
-	_client_addr.sin_addr.s_addr = INADDR_ANY;
-	_client_addr.sin_port = htons(port);
+IRC_Client::IRC_Client(int socket_client): _socket_client(socket_client)
+{
+    std::cout<<"cocket constructeur == "<< socket_client <<" socket dans le client : " << _socket_client<<std::endl;
+    // _socket_client = socket(AF_INET, SOCK_STREAM, 0);
+	// if (_socket_client== -1)
+	// 	throw ThrowException("ERROR SOCKET");
+	// memset(&_client_addr, 0, sizeof(_client_addr));
+	// _client_addr.sin_family = AF_INET;
+	// _client_addr.sin_addr.s_addr = INADDR_ANY;
+	// _client_addr.sin_port = htons(port);
 }
 
-IRC_Client::IRC_Client(const IRC_Client &)
+IRC_Client::IRC_Client(const IRC_Client &client): _socket_client(client._socket_client)
 {}
 
 IRC_Client::~IRC_Client()
 {
-    if (_socket_client != -1)
-        close(_socket_client);
+    std::cout<<"client destructeur"<<std::endl;
+    // if (_socket_client != -1)
+    //     close(_socket_client);
 }
 
 
@@ -28,6 +30,7 @@ struct	sockaddr_in IRC_Client::get_client_addr(void)
 
 int     IRC_Client::get_socket_client(void)
 {
+    std::cout<<"dans le getter "<<_socket_client<<std::endl;
     return (_socket_client);
 }
 
---
+++ IRC_Client.d
@@ -0,0 +1,2 @@
+IRC_Client.o: IRC_Client.cpp IRC_Client.hpp IRC_Server.hpp \
+ IRC_Channel.hpp
