a8b0335566cb542da4c8dffe9980e783a2ba214a
mathieu <mathieu.daguenet@outlook.com>
2024-10-24 00:44:53 +0200

    plein de bonne chose

--- IRC_Client.cpp
+++ IRC_Client.cpp
@@ -1,8 +1,8 @@
 #include "IRC_Client.hpp"
 
-IRC_Client::IRC_Client(int socket_client): _socket_client(socket_client)
+IRC_Client::IRC_Client(int socket_client): _socket_client(socket_client), _client_confirmation(false)
 {
-    std::cout<<"cocket constructeur == "<< socket_client <<" socket dans le client : " << _socket_client<<std::endl;
+    // std::cout<<"cocket constructeur == "<< socket_client <<" socket dans le client : " << _socket_client<<std::endl;
     // _socket_client = socket(AF_INET, SOCK_STREAM, 0);
 	// if (_socket_client== -1)
 	// 	throw ThrowException("ERROR SOCKET");
@@ -12,30 +12,61 @@ IRC_Client::IRC_Client(int socket_client): _socket_client(socket_client)
 	// _client_addr.sin_port = htons(port);
 }
 
-IRC_Client::IRC_Client(const IRC_Client &client): _socket_client(client._socket_client)
+IRC_Client::IRC_Client(const IRC_Client &client): _socket_client(client._socket_client),  _client_confirmation(false)
 {}
 
 IRC_Client::~IRC_Client()
 {
-    std::cout<<"client destructeur"<<std::endl;
+    // std::cout<<"client destructeur"<<std::endl;
     // if (_socket_client != -1)
     //     close(_socket_client);
 }
 
 
-struct	sockaddr_in IRC_Client::get_client_addr(void)
-{
-    return (_client_addr);
-}
+// struct	sockaddr_in IRC_Client::get_client_addr(void)
+// {
+//     return (_client_addr);
+// }
 
 int     IRC_Client::get_socket_client(void)
 {
-    std::cout<<"dans le getter "<<_socket_client<<std::endl;
+    // std::cout<<"dans le getter "<<_socket_client<<std::endl;
     return (_socket_client);
 }
 
 void    IRC_Client::set_socket_client(int socket_client)
 {
-    std::cout << "socket client : " << socket_client << std::endl;
+    // std::cout << "socket client : " << socket_client << std::endl;
     _socket_client = socket_client;
+}
+
+
+void    IRC_Client::set_username(const std::string &username)
+{
+    _username = username;
+}
+
+std::string    IRC_Client::get_username()
+{
+    return (_username);
+}
+
+void    IRC_Client::set_nickname(const std::string &nickname)
+{
+    _nickname = nickname;
+}
+
+std::string    IRC_Client::get_nickname()
+{
+    return (_nickname);
+}
+
+void    IRC_Client::set_client_confirmation(bool value)
+{
+    _client_confirmation = value;
+}
+
+bool    IRC_Client::get_client_confirmation()
+{
+    return (_client_confirmation);
 }
--- IRC_Server.cpp
+++ IRC_Server.cpp
@@ -20,7 +20,7 @@ IRC_Server::IRC_Server(int port, std::string const& password) : _port(port), _pa
 	_socket = socket(AF_INET, SOCK_STREAM, 0);
 	if (_socket == -1)
 		throw ThrowException("ERROR SOCKET");
-	std::cout<<"socket fd = "<<_socket<<std::endl;
+	// std::cout<<"socket fd = "<<_socket<<std::endl;
 	memset(&_server_addr, 0, sizeof(_server_addr));
 	_server_addr.sin_family = AF_INET;
 	_server_addr.sin_addr.s_addr = INADDR_ANY;
@@ -107,88 +107,119 @@ IRC_Server&	IRC_Server::operator=(IRC_Server const& rhs)
 // 	}
 // }
 
+void	IRC_Server::check_socket_server()
+{
+	if (FD_ISSET(_socket, &_readfds))
+	{
+		int client_fd;
+		client_fd = accept(this->_socket, NULL, NULL);
+		std::cout<<"accept client fd: "<<client_fd<<std::endl;
+		if (client_fd == -1)
+			throw ThrowException("ACCEPT ERROR");//enqueter pas quitter??
+		this->_clients.push_back(client_fd);
+	}
+}
+
+
+void	IRC_Server::write_socket_client(int i)
+{
+	if (_clients[i].get_client_confirmation() == false)
+	{
+		send(this->_clients[i].get_socket_client(), ":server CAP * LS :\n:server 001 madaguen :Welcome to the IRC Network madaguen!madaguen@localhost\n:server 002 madaguen :Your host is server, running version 1.0\n:server 003 madaguen :This server was created 2024-10-23\n:server 004 madaguen server 1.0 i nt\n", strlen(":server CAP * LS :\n:server 001 madaguen :Welcome to the IRC Network madaguen!madaguen@localhost\n:server 002 madaguen :Your host is server, running version 1.0\n:server 003 madaguen :This server was created 2024-10-23\n:server 004 madaguen server 1.0 i nt\n"), 0);
+		_clients[i].set_client_confirmation(true);
+	}
+	else
+		send(this->_clients[i].get_socket_client(), "MODE madaguen +i\n", strlen("MODE madaguen +i\n"), 0);
+	// send(this->_clients[i].get_socket_client(), "coucou la socket\n", strlen("coucou la socket\n"), 0);
+}
+
+void	IRC_Server::read_socket_client(int i)
+{
+	char	buffer[2048];
+
+	memset(&buffer, 0, sizeof(buffer));
+	ssize_t	bytes_received = recv(this->_clients[i].get_socket_client(), buffer, sizeof(buffer), 0);
+	if (bytes_received > 0)
+	{
+		std::cout << buffer << std::endl;
+	}
+	else if (bytes_received == 0)
+	{
+		FD_CLR(this->_clients[i].get_socket_client(), &_readfds);
+		this->_clients.erase(this->_clients.begin() + i);
+		std::cout << "Connexion closed" << std::endl;
+	}
+	else
+		throw ThrowException("RECV ERROR");//ne pas quitter?
+}
+
+void	IRC_Server::check_socket_client()
+{
+	for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
+	{
+		if (FD_ISSET(this->_clients[i].get_socket_client(), &_exceptfds))
+		{
+			throw std::runtime_error("error in socket client");//ne pas quitter jsute degager le client
+		}
+		if (FD_ISSET(this->_clients[i].get_socket_client(), &_writefds))
+		{
+			this->write_socket_client(i);
+		}
+		if (FD_ISSET(this->_clients[i].get_socket_client(), &_readfds))
+		{
+			this->read_socket_client(i);
+		}
+	}
+
+}
+
+void	IRC_Server::check_all_sockets()
+{
+	check_socket_server();
+	check_socket_client();
+}
+
+void	IRC_Server::manage_fdset()
+{
+    FD_ZERO(&_readfds);
+    FD_ZERO(&_writefds);
+    FD_ZERO(&_exceptfds);
+    FD_SET(_socket, &_readfds);
+    FD_SET(_socket, &_writefds);
+    FD_SET(_socket, &_exceptfds);
+	for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
+    {
+    	FD_SET(this->_clients[i].get_socket_client(), &_readfds);
+    	FD_SET(this->_clients[i].get_socket_client(), &_writefds);
+    	FD_SET(this->_clients[i].get_socket_client(), &_exceptfds);
+    }
+}
+
+int	IRC_Server::get_nfds()
+{
+	int	maxi_fds = _socket;
+	for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
+	{
+		if (this->_clients[i].get_socket_client() > maxi_fds)
+			maxi_fds = this->_clients[i].get_socket_client();
+	}
+	return (maxi_fds);
+}
+
 void	IRC_Server::manage()
 {
-	char 		buffer[2048];
-	fd_set		readfds;
-	fd_set		writefds;
-	fd_set		exceptfds;
-
 	if (bind(_socket, (struct sockaddr *)&_server_addr, sizeof(_server_addr)) == -1)
 		throw ThrowException("BIND ERROR");
-	if (listen(_socket, 1020) == -1)
+	if (listen(_socket, 1010) == -1)
 		throw ThrowException("LISTEN ERROR");
 	while (true)
 	{
-		// std::cout<<"debut de boucel server: nb client == "<< _clients.size() <<std::endl;
-		memset(&buffer, 0, sizeof(buffer));
-        FD_ZERO(&readfds);
-        FD_ZERO(&writefds);
-        FD_ZERO(&exceptfds);
-        FD_SET(_socket, &readfds);
-        FD_SET(_socket, &writefds);
-        FD_SET(_socket, &exceptfds);
-        for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
-        {
-        	FD_SET(this->_clients[i].get_socket_client(), &readfds);
-        	FD_SET(this->_clients[i].get_socket_client(), &writefds);
-        	FD_SET(this->_clients[i].get_socket_client(), &exceptfds);
-        }
-		int maxi_fds = _socket;
-		for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
-		{
-			std::cout<<"fd client == "<<this->_clients[i].get_socket_client()<<std::endl;
-			if (this->_clients[i].get_socket_client() > maxi_fds)
-				maxi_fds = this->_clients[i].get_socket_client();
-		}
-		std::cout<<"maxi fd == "<<maxi_fds<<std::endl;
-		if (select(maxi_fds + 1, &readfds, NULL, &exceptfds, NULL) == -1)
+		this->manage_fdset();
+		if (select(this->get_nfds() + 1, &_readfds, NULL /*remettre writefds quand on gerera le buffer client */, &_exceptfds, NULL) == -1)
 		{
 			dprintf(2, "eroror select == %s\n", strerror(errno));
 			throw ThrowException("SELECT ERROR");//enqueter quitter ou pas? continue?
 		}
-		if (FD_ISSET(_socket, &readfds))
-		{
-			int client_fd;
-			// struct sockaddr_in client_address;
-    		// socklen_t client_address_len = sizeof(client_address);
-			client_fd = accept(this->_socket, NULL, NULL);
-			std::cout<<"accept client fd: "<<client_fd<<std::endl;
-			if (client_fd == -1)
-				throw ThrowException("ACCEPT ERROR");//enqueter pas quitter??
-			// IRC_Client *client = new IRC_Client(client_fd);
-			// std::cout<<"le fd du lient est : "<<client->get_socket_client()<<std::endl;
-			this->_clients.push_back(client_fd);
-			for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
-			{
-				std::cout<<"fd dans le vecteur == "<<this->_clients[i].get_socket_client()<<std::endl;
-			}
-		}
-		for (int i = 0; i < static_cast<int>(this->_clients.size()); i++)
-		{
-			std::cout<<"chck client set : "<<i<<std::endl;
-			if (FD_ISSET(this->_clients[i].get_socket_client(), &exceptfds))
-				throw std::runtime_error("error in socket client");//ne pas quitter jsute degager le client
-			if (FD_ISSET(this->_clients[i].get_socket_client(), &writefds))
-			{
-				send(this->_clients[i].get_socket_client(), "coucou la socket\n", strlen("coucou la socket\n"), 0);
-			}
-			if (FD_ISSET(this->_clients[i].get_socket_client(), &readfds))
-			{
-				ssize_t	bytes_received = recv(this->_clients[i].get_socket_client(), buffer, sizeof(buffer), 0);
-				if (bytes_received > 0)
-				{
-					std::cout << buffer << std::endl;
-					// send(this->_clients[i].get_socket_client(), "coucou la socket\n", strlen("coucou la socket\n"), 0);
-				}
-				else if (bytes_received == 0)
-				{
-					// FD_CLR(this->_clients[i].get_socket_client(), &readfds);
-					std::cout << "Connexion closed" << std::endl;
-				}
-				else
-					throw ThrowException("RECV ERROR");
-			}
-		}
+		this->check_all_sockets();
 	}
 }
